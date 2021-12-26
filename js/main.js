bb4c8a5f3eb4036d828075f1ce66f76b7b741133
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2021-12-26 22:22:09 +0300

    fixing:menu, adding:content

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+
+});
+const swiper_ = new Swiper('.channel-slider_', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next_',
+    prevEl: '.channel-button-prev_',
+  },
+
+});
+const swiper__ = new Swiper('.channel-slider__', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next__',
+    prevEl: '.channel-button-prev__',
+  },
+
+});
